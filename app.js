d58fdb3b8477cb817ea386768fc8ab688eccab0b
Eduardo Afonso <eduardoafonsodev@gmail.com>
2025-01-28 13:46:12 -0300

    Adicionando amigos no array

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listadeamigos = [];
+
+function adicionarAmigo() {
+    let nomeamigo = document.getElementById('amigo').value;
+    if (nomeamigo != '') {
+
+        addarrayamigos(nomeamigo);
+        console.log(listadeamigos);
+       
+    } else {
+        alert("Digite um nome válido!");
+    }
+
+}
+
+function addarrayamigos(novoamigo) {
+    listadeamigos.push(novoamigo);
+}
